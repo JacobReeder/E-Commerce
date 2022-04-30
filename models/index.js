43feb3685dff6231b7ddecc9a698144222f3969b
JacobReeder <jacob.reeder1@gmail.com>
2022-04-29 19:52:58 -0700

    imported sequelize//server not functional

--- models/index.js
+++ models/index.js
@@ -4,6 +4,9 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+
+///TODO: 13.3.5 explains 'associations', use to solve below
+
 // Products belongsTo Category
 
 // Categories have many Products
